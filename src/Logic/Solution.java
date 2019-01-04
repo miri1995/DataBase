fce736af1be6915bbd1d642161cb8f8e82386b9b
miri1995 <miriasher@gmail.com>
2019-01-04 19:20:45 +0200

    connect all

--- src/Logic/Solution.java
+++ src/Logic/Solution.java
@@ -6,26 +6,19 @@ import java.util.List;
 
 public class Solution {
 
-   // private String artistName;
+
     private  List<String> allArtists;
-
-
 
     public Solution(List<String> allArtists)
     {
-        allArtists = new ArrayList<String>();
+        this.allArtists = new ArrayList<String>();
         this.allArtists=allArtists;
-
-      /*  for(int i=0;i<(allArtists.size()-1);i++){
-            artistName=allArtists.get(i);
-        }*/
-       // this.artistName=artistName;
         System.out.print(allArtists);
 
     }
 
     public Solution(){
-       // this.allArtists = getArtistName();
+        this.allArtists = getArtistName();
     }
 
     public List<String> getArtistName() {
