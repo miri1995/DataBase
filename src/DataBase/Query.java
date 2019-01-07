94ac2c52b12e464732749609a1a74589f9b17cd1
miri1995 <miriasher@gmail.com>
2019-01-07 15:00:18 +0200

    fix all

--- src/DataBase/Query.java
+++ src/DataBase/Query.java
@@ -13,7 +13,8 @@ public class Query {
                 "from genre\n" +
                 "INNER JOIN genreartists on genre.genre_id = genreartists.genre_id\n" +
                 "INNER JOIN artists on artists.artist_id = genreartists.artist_id\n" +
-                "INNER JOIN databaseproject.songs ON artists.artist_id=songs.song_artist_id\n";
+                "WHERE artists.artist_id IN ";
+        String notNull=" AND songs.song_loudness IS NOT NULL AND songs.song_tempo IS NOT NULL";
         switch (tempo){
             case "Slow":
                 temp=1;
@@ -30,43 +31,52 @@ public class Query {
             case "Weak":
                 if(temp==1){
                     //q2;
-                    q=  mapGenre+ "WHERE song_tempo<85 AND song_loudness>-16";
+                    q=  mapGenre+ "(Select song_artist_id FROM songs where song_tempo < 85 AND song_loudness > -16"+
+                            notNull+")";
                 }else if(temp==2){
                     // q5;
                     q= mapGenre+
-                            "WHERE song_tempo between 85 and 170 AND song_loudness>-16";
+                            "(Select song_artist_id FROM songs WHERE song_tempo between 85 and 170 AND song_loudness>-16"+
+                            notNull+")";
                 }else
                     // q8;
                     q= mapGenre+
-                            "WHERE song_tempo>170 AND song_loudness>-16";
+                            "(Select song_artist_id FROM songs WHERE song_tempo>170 AND song_loudness>-16"+
+                            notNull+")";
                 break;
             case "Normal":
                 if(temp==1){
                     // q3;
                     q= mapGenre+
-                            "WHERE song_tempo<85 and 170 AND song_loudness BETWEEN -32 and -16";
+                            "(Select song_artist_id FROM songs WHERE song_tempo<85 and 170 AND song_loudness BETWEEN -32 and -16"+
+                            notNull+")";
                 }else if(temp==2){
                     //  q6;
                     q= mapGenre+
-                            "WHERE song_tempo between 85 and 170 AND song_loudness BETWEEN -32 and -16";
+                            "(Select song_artist_id FROM songs WHERE song_tempo between 85 and 170 AND song_loudness BETWEEN -32 and -16"+
+                            notNull+")";
                 }else
                     //  q9;
                     q= mapGenre+
-                            "WHERE song_tempo>170 AND song_loudness BETWEEN -32 and -16";
+                            "(Select song_artist_id FROM songs WHERE song_tempo>170 AND song_loudness BETWEEN -32 and -16"+
+                            notNull+")";
                 break;
             case "Strong":
                 if(temp==1){
                     // q4;
                     q= mapGenre+
-                            "WHERE song_tempo<85 AND not song_loudness>-32";
+                            "(Select song_artist_id FROM songs WHERE song_tempo<85 AND not song_loudness>-32"+
+                            notNull+")";
                 }else if(temp==2){
                     // q7;
                     q= mapGenre+
-                            "WHERE song_tempo between 85 and 170 AND not song_loudness>-32";
+                            "(Select song_artist_id FROM songs WHERE song_tempo between 85 and 170 AND not song_loudness>-32"+
+                            notNull+")";
                 }else
                     //q10;
                     q= mapGenre+
-                            "WHERE song_tempo>170 AND not song_loudness>-32";
+                            "(Select song_artist_id FROM songs WHERE song_tempo>170 AND not song_loudness>-32"+
+                            notNull+")";
                 break;
         }
         String sol=GetSol(q,genre);
@@ -80,10 +90,8 @@ public class Query {
 
     public String GetSol(String BeatQ, String genre){
         String hotness=" order by artists.artist_hotness DESC";
-        String notNull=" AND songs.song_loudness IS NOT NULL AND songs.song_tempo IS NOT NULL";
+        String lastQ=BeatQ+" AND genre.genre=\""+genre+"\""+hotness;
 
-        String lastQ=BeatQ+" AND genre.genre=\""+genre+"\""+notNull+hotness;
-        //String lastQ=BeatQ;
         return lastQ;
     }
 
